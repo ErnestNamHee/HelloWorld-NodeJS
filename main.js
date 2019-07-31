2d0ba66981bc0ada9f6aebcf18397a8a8011d611
ErnestNamHee <37908030+ErnestNamHee@users.noreply.github.com>
2019-07-31 18:21:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 // load the http module
 var http = require('http');
+var fs = require('fs'); 
+
+function send404(response) { 
+    response.writeHead(404, { 'Content-Type': 'text/plain' }); 
+    response.write('Error 404: Resource not found.'); 
+    response.end(); 
+} 
 
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello, flexible engine, this is the brandnew build #98 \n");
+  if (request.method == 'GET' && request.url == '/') {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    fs.createReadStream('index.html').pipe(response);
+  } else { 
+    send404(response);
+  }
 });
 
 // listen on localhost:8000
